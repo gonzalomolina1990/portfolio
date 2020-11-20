178476f9639e3de56132109d50ec9ce519cc704a
gonzalomolina1990 <61057264+gonzalomolina1990@users.noreply.github.com>
2020-11-20 08:31:14 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-$("#designerContainer").on("click", function () {
-  console.log("click");
-  $(".jobs").css("display", "block");
+window.addEventListener("scroll", function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $(".navbar").css("background-color", " rgba(0, 0, 0, .7)") = "black";
+  } else if (!window.matchMedia("(max-width: 990px)").matches) {
+    $(".navbar").css("background-color", " rgba(53, 53, 53, 0)") = "black";
+  }
 });
